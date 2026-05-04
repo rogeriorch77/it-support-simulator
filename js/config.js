d977ea09569e5be08b63f73e484d7fd511b8f071
rogeriorch77 <rogeriorocha.dev@gmail.com>
2026-05-04 17:09:15 +0200

    fix: correct Supabase URL

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Project Settings
-const SUPABASE_URL  = 'https://https://lwubldeubynnbcfzmhrx.supabase.co.supabase.co';
+const SUPABASE_URL  = 'https://lwubldeubynnbcfzmhrx.supabase.co';
 const SUPABASE_ANON = 'e8605ca86b3869cc4e152063a9a74fa1230700ab';
